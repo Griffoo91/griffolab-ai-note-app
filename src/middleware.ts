b565e79f9df074e578feafb962b2dd7cb5e3ce5f
Griffoo91 <omboggorised91@gmail.com>
2024-04-23 17:02:42 +0300

    7th commit:Clerk authentication set up

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
+
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+  });
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+    '/forum(.*)',
+    '/notes(.*)',
+  ]);
+   
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
