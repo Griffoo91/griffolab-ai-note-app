b18980a11c833fc64b8809e303844dffffc88ff6
Griffoo91 <omboggorised91@gmail.com>
2024-04-24 14:58:46 +0300

    12th commit: prisma schema type, addnotes settings and userrequest and response inclusion

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,12 +1,18 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/assets/Griffobg0.png";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddNoteDialog from "@/components/addNoteDialog";
 
 export default function NavBar() {
+    const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
   return (
+    <>
     <div className="p-4 shadow">
       <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
         <Link href="/notes" className="flex items-center gap-1">
@@ -27,5 +33,7 @@ export default function NavBar() {
         </div>
       </div>
     </div>
+    {showAddNoteDialog && <AddNoteDialog open={showAddNoteDialog} setOpen={setShowAddNoteDialog} />}
+    </>
   );
 }
--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,7 +1,14 @@
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-    title: "GriffoLab - notes",
-  };
-export default function NotesPage(){
-    return<div>Here will be your notes</div>
+  title: "GriffoLab - notes",
+};
+export default async function NotesPage() {
+  const { userId } = auth();
+
+  if (!userId) throw Error("userId undefined");
+
+  const allNotes = await prisma.note.findMany({ where: {userId} })
+  return <div>{JSON.stringify(allNotes)}</div>;
 }
