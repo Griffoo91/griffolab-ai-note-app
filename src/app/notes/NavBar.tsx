abc507b874d1fd9e649f80654cd4b03fcd6a6dbf
Griffoo91 <omboggorised91@gmail.com>
2024-04-24 12:22:09 +0300

    10th commit: avatarbox inclusion from shadcn ui components and resize

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/assets/Griffobg0.png"
+import Logo from "@/assets/Griffobg0.png";
+import { UserButton } from "@clerk/nextjs";
 
 export default function NavBar() {
-  return <div className="p-4 shadow">
-    <div className="m-auto max-w-7xl flex flex-wrap gap-3 items-center justify-between">
-        <Link href="/notes" className="flex items-center gap-1" >
-            <Image src={Logo} alt="GriffoLab logo" width={40} height={40} />
-            <span className="font-bold">GriffoLabAi</span>
+  return (
+    <div className="p-4 shadow">
+      <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
+        <Link href="/notes" className="flex items-center gap-1">
+          <Image src={Logo} alt="GriffoLab logo" width={40} height={40} />
+          <span className="font-bold">GriffoLabAi</span>
         </Link>
-        <span>Element 2</span>
+        <div className="flex items-center gap-2">
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+            }}
+          />
+        </div>
+      </div>
     </div>
-    </div>;
+  );
 }
