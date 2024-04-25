39fb894351e9619bdd14557e68a7e807331938f3
Griffoo91 <omboggorised91@gmail.com>
2024-04-25 19:04:53 +0300

    14th commit:Note title form component from shadcn ui

--- src/components/addNoteDialog.tsx
+++ src/components/addNoteDialog.tsx
@@ -2,6 +2,15 @@ import { CreateNoteSchema, createNoteSchema } from "@/lib/validation/note";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
 
 interface AddNoteDialogProps {
   open: boolean;
@@ -14,14 +23,31 @@ export default function AddNoteDialog({ open, setOpen }: AddNoteDialogProps) {
   async function onSubmit(input: CreateNoteSchema) {
     alert(input);
   }
-  
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add Notes</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
