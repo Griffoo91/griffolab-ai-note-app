5320213604882def1c6c8f66972790958b579c0a
Griffoo91 <omboggorised91@gmail.com>
2024-04-29 17:38:40 +0300

    commit: rename of the addEditNoteDialog from addNoteDialog, style of the note component

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,30 +1,51 @@
-import { Note as NoteModel } from "@prisma/client";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+"use client";
 
-interface noteProps{
-    note: NoteModel
+import { Note as NoteModel } from "@prisma/client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { useState } from "react";
+import AddEditNoteDialog from "./addEditNoteDialog";
+
+interface noteProps {
+  note: NoteModel;
 }
 
-export default function Note({note}: noteProps) {
+export default function Note({ note }: noteProps) {
+  const [showEditDialog, setShowEditDialog] = useState(false);
 
-    const wasUpdated = note.updateAt > note.createdAt;
+  const wasUpdated = note.updateAt > note.createdAt;
 
-    const createdUpdatedAtTimestamp = (
-        wasUpdated ? note.updateAt : note.createdAt
-    ).toDateString()
+  const createdUpdatedAtTimestamp = (
+    wasUpdated ? note.updateAt : note.createdAt
+  ).toDateString();
 
-    return(
-        <Card>
-            <CardHeader>
-                <CardTitle>{note.title}</CardTitle>
-                <CardDescription>
-                    {createdUpdatedAtTimestamp}
-                    {wasUpdated && " (updated)"}
-                    </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <p className="whitespace-pre-line">{note.content}</p>
-            </CardContent>
-        </Card>
-    )
+  return (
+    <>
+      <Card
+        className="cursor-pointer transition-shadow hover:shadow-lg"
+        onClick={() => setShowEditDialog(true)}
+      >
+        <CardHeader>
+          <CardTitle>{note.title}</CardTitle>
+          <CardDescription>
+            {createdUpdatedAtTimestamp}
+            {wasUpdated && " (updated)"}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="whitespace-pre-line">{note.content}</p>
+        </CardContent>
+      </Card>
+      <AddEditNoteDialog
+        open={showEditDialog}
+        setOpen={setShowEditDialog}
+        noteToEdit={note}
+      />
+    </>
+  );
 }
