e2ecb640ea67c7767ca68337edf595a8cc7d8a10
Griffoo91 <omboggorised91@gmail.com>
2024-04-30 19:04:07 +0300

    35th commit: Clerk user login on the panel theme included

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -8,34 +8,41 @@ import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useState } from "react";
 import AddNoteDialog from "@/components/addEditNoteDialog";
+import { dark } from "@clerk/themes";
 import ThemeToggleButton from "@/components/ThemeToggleButton";
+import { useTheme } from "next-themes";
 
 export default function NavBar() {
-    const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
+  const { theme } = useTheme();
+  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
   return (
     <>
-    <div className="p-4 shadow">
-      <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
-        <Link href="/notes" className="flex items-center gap-1">
-          <Image src={Logo} alt="GriffoLab logo" width={40} height={40} />
-          <span className="font-bold">GriffoLabAi</span>
-        </Link>
-        <div className="flex items-center gap-2">
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
-            }}
-          />
-          <ThemeToggleButton />
-          <Button onClick={() => setShowAddEditNoteDialog(true)}>
-            <Plus size={20} className="mr-2"/>
-            Add Note
-          </Button>
+      <div className="p-4 shadow">
+        <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
+          <Link href="/notes" className="flex items-center gap-1">
+            <Image src={Logo} alt="GriffoLab logo" width={40} height={40} />
+            <span className="font-bold">GriffoLabAi</span>
+          </Link>
+          <div className="flex items-center gap-2">
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                baseTheme: (theme === "dark" ? dark : undefined),
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
+            <ThemeToggleButton />
+            <Button onClick={() => setShowAddEditNoteDialog(true)}>
+              <Plus size={20} className="mr-2" />
+              Add Note
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
-    <AddNoteDialog open={showAddEditNoteDialog} setOpen={setShowAddEditNoteDialog} />
+      <AddNoteDialog
+        open={showAddEditNoteDialog}
+        setOpen={setShowAddEditNoteDialog}
+      />
     </>
   );
 }
